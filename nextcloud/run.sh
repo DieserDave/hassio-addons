4f4e2da19b95ae42019c2062193e4e15c7d33f93
DieserDave <117954926+DieserDave@users.noreply.github.com>
2023-08-13 11:48:17 +0200

    Create run.sh

---
+++ nextcloud/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+SHARE_DIR=/share/nextcloud
+if [ ! -d "${SHARE_DIR}" ]; then
+    mkdir -p "${SHARE_DIR}"
+    mkdir -p "${SHARE_DIR}/html"
+    chown -R www-data:root "${SHARE_DIR}"
+    chmod -R g=u "${SHARE_DIR}"
+fi
+
+/entrypoint.sh "$@"
