6ccb6ddc8fb8b3f305cac55729342d9c7d32b1bc
DieserDave <117954926+DieserDave@users.noreply.github.com>
2023-08-13 14:14:52 +0200

    Update run.sh

--- nextcloud/run.sh
+++ nextcloud/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo 'Hostname:'
+hostname
+echo ''
+
 SHARE_DIR=/share/nextcloud
 if [ ! -d "${SHARE_DIR}" ]; then
     mkdir -p "${SHARE_DIR}"
@@ -9,12 +13,27 @@ if [ ! -d "${SHARE_DIR}" ]; then
     chmod -R g=u "${SHARE_DIR}"
 fi
 
+run_as() {
+    if [ "$(id -u)" = 0 ]; then
+        su -p "$user" -s /bin/sh -c "$1"
+    else
+        sh -c "$1"
+    fi
+}
+
+if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then
+    echo "Setting trusted domains…"
+    NC_TRUSTED_DOMAIN_IDX=1
+    for DOMAIN in $NEXTCLOUD_TRUSTED_DOMAINS ; do
+        DOMAIN=$(echo "$DOMAIN" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        run_as "php $SHARE_DIR/html/occ config:system:set trusted_domains $NC_TRUSTED_DOMAIN_IDX --value=$DOMAIN"
+        NC_TRUSTED_DOMAIN_IDX=$((NC_TRUSTED_DOMAIN_IDX+1))
+    done
+fi
+
 CONFIG_PATH=/data/options.json
 echo 'Starting with the following configuration:';
 jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
 eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
-
-echo 'Hostname:'
-hostname
 
 /entrypoint.sh "$@"
