b5499b1a3e350504269db8f99eecb10f53f49c9c
DieserDave <117954926+DieserDave@users.noreply.github.com>
2026-04-20 13:27:55 +0200

    Update run.sh

--- hetzner_console_dns/run.sh
+++ hetzner_console_dns/run.sh
@@ -5,7 +5,5 @@ export APIKEY="${APIKEY}"
 export ZONENAME="${ZONENAME}"
 export ARECORDNAMES__0="${ARECORDNAMES__0}"
 
-echo $APIKEY > /APIKEY.txt
-
 # Start the Hetzner DynDNS service
 exec /entrypoint.sh
